e042c14efd1c269e02c0626a6c7821650d04cae8
Ethan-BCTC <148492788+Ethan-BCTC@users.noreply.github.com>
2024-01-20 00:54:48 -0500

    just throwing shit at the wall at this point

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('nav');
 
     hamburgerMenu.addEventListener('click', function () {
-        const isNavVisible = nav.style.height === 'auto';
-        nav.style.height = isNavVisible ? '0' : 'auto';
+        nav.classList.toggle('show-nav');
+        hamburgerMenu.classList.toggle('open-menu');
     });
 });
