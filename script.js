9e9f72448594bc0298254c0623a747702e4358bb
Ethan-BCTC <148492788+Ethan-BCTC@users.noreply.github.com>
2024-01-20 12:14:41 -0500

    Lets Try This

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const nav = document.querySelector('nav');
-
-    hamburgerMenu.addEventListener('click', function () {
-        nav.classList.toggle('show-nav');
-    });
-});
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
