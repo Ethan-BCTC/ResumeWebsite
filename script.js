30b07b51cef700b77673e8c5126638f69f691738
Ethan-BCTC <148492788+Ethan-BCTC@users.noreply.github.com>
2024-01-23 10:24:01 -0500

    addEventListener

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-window.onclick = function (event) {
-    var dropdownClicked = event.target.matches('.resume-button');
-    
+// Updated JS
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
     var dropdowns = document.getElementsByClassName("dropdown-content");
+    
     for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (!dropdownClicked && openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
+        if (dropdowns[i] !== dropdown) {
+            dropdowns[i].classList.remove('show');
         }
     }
-
-    if (event.target == modal) {
-        modal.style.display = "none";
+    
+    dropdown.classList.toggle("show");
+}
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.resume-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
+});
+
+// Get the modal
+var modal = document.getElementById("instamodal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("instabtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+      }
+});
