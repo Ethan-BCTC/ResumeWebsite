e47fe2ec50f7e511ed4ed3ff323eb1b877a08425
Ethan-BCTC <148492788+Ethan-BCTC@users.noreply.github.com>
2024-01-24 08:57:00 -0500

    Moved script into html instead of external file

--- script.js
+++ script.js
@@ -22,14 +22,3 @@ window.addEventListener('click', function(event) {
         }
     }
 });
-myButton.addEventListener("click", function () {
-    myPopup.classList.add("show");
-});
-closePopup.addEventListener("click", function () {
-    myPopup.classList.remove("show");
-});
-window.addEventListener("click", function (event) {
-    if (event.target == myPopup) {
-        myPopup.classList.remove("show");
-    }
-});
